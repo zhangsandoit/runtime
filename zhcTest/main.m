40918f2c0690c717906f9feb1aa71e43fb969c0a
ronglian <hczhang01@ronglian.com>
2020-06-12 14:50:47 +0800

    iOS runtime 集成 objc4-781

---
+++ zhcTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  zhcTest
+//
+//  Created by cocoa on 2020/6/12.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/message.h>
+#import <objc/runtime.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+           
+           NSLog(@"Hello, World!");
+       }
+       return 0;
+    
+}
